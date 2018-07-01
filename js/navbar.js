bf985a4da4bd8eab3296aabb40ac81c6360b96e7
NatkanskiMichal <33288489+NatkanskiMichal@users.noreply.github.com>
2018-07-02 00:16:34 +0200

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	if ($(window).width() <= 767) {	
+        $('.header__nav-toggle-icon').click(function () {
+        $(this).toggleClass('rotated-menu-icon');
+        $('ul').slideToggle('active');
+        $('.header__triangle-animation-right').hide('active');
+        $('.header__triangle-animation-left').show('active');
+    });
+   
+    $('li').click(function () {
+        $('ul').slideUp('active');
+        $('header__nav-toggle-icon').toggleClass('rotated-triangle-menu-li');
+         $('.header__triangle-animation-right').show('active');
+        $('.header__triangle-animation-left').hide('active');
+    });
+  
+    $('.header__logo, .home').click(function () {
+         $('.header__triangle-animation-right').show('active');
+        $('.header__triangle-animation-left').show('active');
+    });
+	}
+});
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(".home, .header__logo").on("click", function () {
+    $("body, html").animate({
+        scrollTop: $("header").offset().top
+    }, 500);
+});
+
+$(".header__arrow, .aboutMe").on("click", function () {
+	$("body, html").animate({
+		scrollTop: $(".main__aboutMe").offset().top
+	}, 500);
+});
+
+$(".skillSet").on("click", function () {
+	$("body, html").animate({
+		scrollTop: $(".main__skills").offset().top
+	}, 500);
+});
+
+$(".projects").on("click", function () {
+	$("body, html").animate({
+		scrollTop: $(".main__projects").offset().top
+	}, 500);
+});
+
+$(".contactMe").on("click", function () {
+	$("body, html").animate({
+		scrollTop: $(".main__contact").offset().top
+	}, 500);
+});
